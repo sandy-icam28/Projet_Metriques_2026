8ec784e663e31c33cc27e957ada06874a3310657
sandy-icam28 <sandycraft89116@gmail.com>
2026-04-10 15:09:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,10 @@ def api_paris():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route("/histogramme")
+def mongraphique():
+    return render_template("histogramme.html")
+
 
 # Ne rien mettre après ce commentaire
     
